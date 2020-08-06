c359900a7eda791eb409d780da2a1795348ce496
Mengman <corrsboy@gmail.com>
2020-08-06 15:16:01 +0800

    fix typo

--- cmake/variables/env.cmake
+++ cmake/variables/env.cmake
@@ -1,7 +1,7 @@
 # cmake -P env.cmake
 # Output:
-# -- env_foo_var: a environment varaible
+# -- env_foo_var: an environment varaible
 # 
-set(ENV{env_foo_var} "a environment varaible")
+set(ENV{env_foo_var} "an environment varaible")
 
 message(STATUS "env_foo_var: $ENV{env_foo_var}")
